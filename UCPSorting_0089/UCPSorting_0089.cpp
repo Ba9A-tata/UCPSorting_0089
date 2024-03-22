8daa66c4922bc087e60948a140a9192c2b645097
Ba9A-tata <yogadzakkii@gmail.com>
2024-03-22 13:54:17 +0700

    Add project files.

---
+++ UCPSorting_0089/UCPSorting_0089.cpp
@@ -0,0 +1,15 @@
+// Jawaban
+// No 1. 
+// No 2. Linear dan Non linear
+// No 3.
+// No 4.
+// No 5. Yang termasuk kedalam Quadtaric : bubblesort,selectionsort
+//       Yang termasuk kedalam Loglinear
+// 
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
