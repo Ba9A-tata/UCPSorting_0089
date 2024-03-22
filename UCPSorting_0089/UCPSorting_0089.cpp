e196c020efd20bf272d262589d4e3ce165339ef0
Ba9A-tata <yogadzakkii@gmail.com>
2024-03-22 18:22:07 +0700

    UCPSorting

--- UCPSorting_0089/UCPSorting_0089.cpp
+++ UCPSorting_0089/UCPSorting_0089.cpp
@@ -1,15 +1,19 @@
 // Jawaban
-// No 1. 
-// No 2. Linear dan Non linear
-// No 3.
-// No 4.
-// No 5. Yang termasuk kedalam Quadtaric : bubblesort,selectionsort
-//       Yang termasuk kedalam Loglinear
-// 
-//
-#include <iostream>
+// No 1. Bubble Sort adalah proses algoritma yang membandingkan anatar elemen yang berseblahan, jika elemen sebelah kiri lebih besar dari yang kanan maka akan jadi pertukaran tempat(SWAP),Namun jika sudah tepat maka akan lanjut ke step selanjutnya (Pass)
+// No 2.
+// Jika kita akan menggunakan algortima shell sort hal pertama yang kita harus lakukan adalah memilih 1 elemen dan lewat 1 elemen.
+// Contoh : 0 dengan 2 1 dengan 3 4 dengan 6 dan seterusnya, lalu kita akan membuat sublist dan setelah itu kita akan menempatkan elmen elemen tersebut ke sublistnya
+//  
+// No 3.Kita harus memilih algoritma shell sort dikarenakan didalam algoritma ini biasanya ada beberapa data yang sudah berurutan, maka algoritma ini sangat cocok digunakan untuk pengelolaan data dan juga efisiensi waktu pengerjaan
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[89] // Deklarasi variabel global array a dengan ukuran 89
+int n; // Deklarasi variabel global n untuk menyimpan banyak elemen array
+
+void input() { // Prosedur untuk input
+    cout << "Masukan banyaknya elemen pada array : "; // output ke layar
+    cin >> n; // input dari pengguna
+    if (n <= 89) // jika n kurang dari atau sama dengan 89
